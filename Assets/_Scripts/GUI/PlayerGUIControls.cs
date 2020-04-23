1a0025abbea251e3cce62eb8e2e0cc7509c3c327
Daniel Handojo <daniel_handojo@hotmail.com>
2020-04-22 17:06:55 -0700

    little improvement. Found the button object

--- Assets/_Scripts/GUI/PlayerGUIControls.cs
+++ Assets/_Scripts/GUI/PlayerGUIControls.cs
@@ -23,8 +23,17 @@ class PlayerGUIControls : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Mouse position = " + Input.mousePosition);
-            Debug.Log("Mouse position (world) = " + Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)));
+            Vector3 normalizedPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         }
+    }
+
+    void OnMouseEnter()
+    {
+        Debug.Log("Over button");
+    }
+
+    void OnMouseOver()
+    {
+        Debug.Log("Over button");
     }
 }
